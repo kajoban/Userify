c9f019efa60bbdb3b658238c7fdd511cc151e10c
kajoban <kuhapark@mcmaster.ca>
2020-09-14 02:42:36 -0400

    implement create new user

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 var express = require("express");
 var app = express();
 var db = require("./database.js");
+var md5 = require("md5");
+
+//pre-processing utils for POST reqs
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //start up server
 var HTTP_PORT = 8000;
@@ -14,6 +20,7 @@ app.get("/", (req, res, next) => {
   res.json({ message: "OK" });
 });
 
+//GET all users
 app.get("/api/users", (req, res, next) => {
   var sql = "SELECT * FROM user";
   var params = [];
@@ -31,12 +38,13 @@ app.get("/api/users", (req, res, next) => {
   });
 });
 
+//GET user by id
 app.get("/api/users/:id", (req, res, next) => {
   //dynamically create select statement based on id
   var sql = "SELECT * FROM user WHERE id = ?";
   var params = [req.params.id];
   //.get so we get only 1 row
-  db.all(sql, params, (err, row) => {
+  db.get(sql, params, (err, row) => {
     if (err) {
       res.status(400).json({ error: err.message });
       return;
@@ -44,6 +52,46 @@ app.get("/api/users/:id", (req, res, next) => {
       res.json({
         message: "success",
         data: row,
+      });
+    }
+  });
+});
+
+//CREATE new user
+//can use curL to hit:
+//curl -d "name=x&email=y&password=z" -X POST http://localhost:8000/api/user/
+app.post("/api/user/", (req, res, next) => {
+  //req.body will be converted from string -> js object via body parser
+  //check mandatory fields
+  var errors = [];
+  if (!req.body.password) {
+    errors.push("No password specified");
+  }
+  if (!req.body.email) {
+    errors.push("No email specified");
+  }
+  if (errors.length) {
+    res.status(400).json({ error: errors.join(",") });
+    return;
+  }
+  //create data object
+  var data = {
+    name: req.body.name,
+    email: req.body.email,
+    password: md5(req.body.password),
+  };
+  var sql = "INSERT INTO user (name, email, password) VALUES (?,?,?)";
+  var params = [data.name, data.email, data.password];
+  //use classic function notation so we can gett this.lastID
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: "error.message" });
+      return;
+    } else {
+      res.json({
+        message: "success",
+        data: data,
+        id: this.lastID,
       });
     }
   });
