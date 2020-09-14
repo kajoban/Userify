4c0bf29dfcfdd2bb2466d6985d69f4d6f1556a77
kajoban <kuhapark@mcmaster.ca>
2020-09-14 02:24:57 -0400

    add get via id

--- server.js
+++ server.js
@@ -31,6 +31,24 @@ app.get("/api/users", (req, res, next) => {
   });
 });
 
+app.get("/api/users/:id", (req, res, next) => {
+  //dynamically create select statement based on id
+  var sql = "SELECT * FROM user WHERE id = ?";
+  var params = [req.params.id];
+  //.get so we get only 1 row
+  db.all(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    } else {
+      res.json({
+        message: "success",
+        data: row,
+      });
+    }
+  });
+});
+
 //set default response
 app.use(function (req, res) {
   res.status(404);
